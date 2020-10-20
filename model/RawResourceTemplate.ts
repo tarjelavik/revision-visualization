d3c06bb00573d10256f070df98344dd22f0c83cd
Ahl Nilsen <ahl.nilsen@uib.no>
2020-10-20 10:20:10 +0200

    Add interface for a raw resource template

---
+++ model/RawResourceTemplate.ts
@@ -0,0 +1,16 @@
+export default interface RawResourceTemplate {
+    data: [
+        {
+            '@context': '',
+            '@id': '',
+            '@type': '',
+            'o:id': number,
+            'o:label': '',
+            'o:owner': {},
+            'o:resource_class': {},
+            'o:title_property': {},
+            'o:description_property': {},
+            'o:resource_template_property': []
+        }
+    ]
+}
