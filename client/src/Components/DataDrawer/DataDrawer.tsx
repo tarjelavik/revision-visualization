89b3410310445c0db1a38314a7c82e02e12f2635
Ahl.Nilsen <ahl.nilsen@uib.no>
2020-11-20 11:48:10 +0100

    Add overlay in order to close drawer when clicking outside it.

--- client/src/Components/DataDrawer/DataDrawer.tsx
+++ client/src/Components/DataDrawer/DataDrawer.tsx
@@ -5,6 +5,7 @@ import {
     DrawerFooter,
     DrawerHeader,
     DrawerContent,
+    DrawerOverlay,
     useDisclosure,
     Button
   } from '@chakra-ui/react';
@@ -69,20 +70,23 @@ function DataDrawer(props: any) {
              <Drawer
                 isOpen={props.displayDrawer}
                 placement='right'
-                onClose={onClose}>
-                <DrawerContent>
-                    <DrawerHeader>{displayProperties?.type}</DrawerHeader>
-                    <DrawerBody>
-                    <ul style={listStyle}>
-                        <li style={listElementStyle}>{displayProperties?.name}</li>
-                        <li style={listElementStyle}><a href={displayProperties?.link} target='_blank' rel='noopener noreferrer'>See full resource page</a></li>
-                    </ul>
-                    </DrawerBody>
-                    {drawerIllustration}
-                    <DrawerFooter>
-                        <Button onClick={() => props.setDisplayDrawer(false)}>Close</Button>
-                    </DrawerFooter>
-                </DrawerContent>
+                onClose={onClose}
+                onOverlayClick={() => props.setDisplayDrawer(false)}>
+                <DrawerOverlay bg="none">
+                    <DrawerContent>
+                        <DrawerHeader>{displayProperties?.type}</DrawerHeader>
+                        <DrawerBody>
+                        <ul style={listStyle}>
+                            <li style={listElementStyle}>{displayProperties?.name}</li>
+                            <li style={listElementStyle}><a href={displayProperties?.link} target='_blank' rel='noopener noreferrer'>See full resource page</a></li>
+                        </ul>
+                        </DrawerBody>
+                        {drawerIllustration}
+                        <DrawerFooter>
+                            <Button onClick={() => props.setDisplayDrawer(false)}>Close</Button>
+                        </DrawerFooter>
+                    </DrawerContent>
+                </DrawerOverlay>
             </Drawer>
             : null}
         </div>
