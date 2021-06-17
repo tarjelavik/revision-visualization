72c7132051773c2134d17fad9c71a51f1f47f378
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-06-17 18:19:13 +0200

    Improve states on Sigmabox

--- components/SigmaBox/index.js
+++ components/SigmaBox/index.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Stack, Center, Heading } from '@chakra-ui/react';
+import { Box, Heading, Flex } from '@chakra-ui/react';
 import {Sigma, RandomizeNodePositions, NOverlap, RelativeSize, DragNodes, ForceAtlas2 } from 'react-sigma';
 import SigmaLoader from './SigmaLoader';
-import IllustrationContainer from '../IllustrationContainer/IllustrationContainer';
-import { getActionId } from '../../pages/api/lib/helpers';
-
-// const noResults = <IllustrationContainer src={ASSSET} alt='No results' heigth='800px' width='800px'/>;
 
 const sigmaStyle = {
   height: '100vh',
@@ -35,80 +31,75 @@ const SigmaBox = ({classes, getClickedNodeData, setDisplayDrawer}) => {
     setGraph(body.graph);
     setLoading(false);
   };
-
+  
   useEffect(() => {
     getGraph();
   }, [classes]);
 
   return (
     <>
-    {graph?.nodes && !loading &&
-      <Box w='100%' h='100%'>
-        <Sigma
-          style={sigmaStyle}
-          onClickEdge={(edgeEvent) => onClickEdgeHandler(edgeEvent)}
-          onClickNode={(nodeEvent) => onClickNodeHandler(nodeEvent)}
-          renderer='canvas'
-          settings={{
-            sideMargin: 50,
-            defaultLabelSize: 14,
-            labelThreshold: 0,
-            drawEdgeLabels: true,
-            drawLabels: true,
-            minArrowSize: 8,
-            clone: false,
-            minNodeSize: 16,
-            defaultNodeColor: '#454554',
-            enableEdgeHovering: true,
-            edgeHoverSizeRatio: 3,
-            edgeHoverPrecision: 5,
-            minEdgeSize: 1,
-            verbose: true
-          }}
-        >
-          <SigmaLoader graph={graph}>
-            <ForceAtlas2/>
-            <NOverlap gridSize={1} maxIterations={1000}/>
-            <RandomizeNodePositions/>
-            <DragNodes
-              // tslint:disable-next-line:no-empty
-              onDrag={function noRefCheck() {}}
-              // tslint:disable-next-line:no-empty
-              onDragend={function noRefCheck() {}}
-              // tslint:disable-next-line:no-empty
-              onDrop={function noRefCheck() {}}
-              // tslint:disable-next-line:no-empty
-              onStartdrag={function noRefCheck() {}}
-            />
-            <RelativeSize initialSize={10}/>
-          </SigmaLoader>
-        </Sigma>
-
-      </Box>}
-
       {loading &&
-        <Center>
-          <Stack direction='column'>
-            <Box>
-              <Heading as='h1' size='2xl' textAlign='center'>Loading data...</Heading>
-            </Box>
-          </Stack>
-        </Center>
-      } 
+        <Flex
+          h="80vh"  
+          justifyContent="center"
+          align="center"
+        >
+          <Heading as='p' size='xl'>Loading data...</Heading>
+        </Flex>} 
 
-      {!graph.node && !loading && ( 
-        <Center>
-          <Stack direction='column'>
-            {/* <Box>
-              {noResults}
-            </Box> */}
-            <Box>
-              <Heading as='h1' size='2xl' textAlign='center'>No results!</Heading>
-              <Heading as='h2' size='xl' mt='4rem' textAlign='center'>Refresh the page to search again</Heading>
-            </Box>
-          </Stack>
-        </Center>
-      )}
+      {graph?.nodes && !loading &&
+        <Box w='100%' h='100%'>
+          <Sigma
+            style={sigmaStyle}
+            onClickEdge={(edgeEvent) => onClickEdgeHandler(edgeEvent)}
+            onClickNode={(nodeEvent) => onClickNodeHandler(nodeEvent)}
+            renderer='canvas'
+            settings={{
+              sideMargin: 50,
+              defaultLabelSize: 14,
+              labelThreshold: 0,
+              drawEdgeLabels: true,
+              drawLabels: true,
+              minArrowSize: 8,
+              clone: false,
+              minNodeSize: 16,
+              defaultNodeColor: '#454554',
+              enableEdgeHovering: true,
+              edgeHoverSizeRatio: 3,
+              edgeHoverPrecision: 5,
+              minEdgeSize: 1,
+              verbose: true
+            }}
+          >
+            <SigmaLoader graph={graph}>
+              <ForceAtlas2/>
+              <NOverlap gridSize={1} maxIterations={1000}/>
+              <RandomizeNodePositions/>
+              <DragNodes
+                // tslint:disable-next-line:no-empty
+                onDrag={function noRefCheck() {}}
+                // tslint:disable-next-line:no-empty
+                onDragend={function noRefCheck() {}}
+                // tslint:disable-next-line:no-empty
+                onDrop={function noRefCheck() {}}
+                // tslint:disable-next-line:no-empty
+                onStartdrag={function noRefCheck() {}}
+              />
+              <RelativeSize initialSize={10}/>
+            </SigmaLoader>
+          </Sigma>
+        </Box>}
+
+        {graph.nodes && !loading && ( 
+          <Flex
+            h="80vh"  
+            justifyContent="center"
+            align="center"
+          >
+            <Heading as='h1' size='2xl' textAlign='center'>No results!</Heading>
+            <Heading as='h2' size='xl' mt='4rem' textAlign='center'>Refresh the page to search again</Heading>
+          </Flex>
+        )}
     </>
   );
 };
