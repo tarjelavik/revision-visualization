c767ff6f4e0f304783779e23995470901131d740
tarjelavik <Tarje.Lavik@ub.uib.no>
2021-06-21 09:54:55 +0200

    Remove tests

--- pages/api/graph/__tests__/generateId.test.ts
+++
@@ -1,6 +0,0 @@
-import { generateId } from '../../lib/generateId';
-
-test('generateId should not return undefined', () => {
-    const id = generateId();
-    expect(id).not.toBeUndefined();
-});
--- pages/api/graph/__tests__/helpers.test.ts
+++
@@ -1,6 +0,0 @@
-import { getActionId } from '../../lib/helpers';
-
-test('getActionId should extract only numbers from an URL', () => {
-    const actionId = getActionId('https://dummy.url.net/1337');
-    expect(actionId).toBe(1337);
-});
