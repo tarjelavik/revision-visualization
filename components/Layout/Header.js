f2b0135da1c86d3be407a9d5b21a0348fd0c8785
Tarje Lavik <tarje.lavik@gmail.com>
2023-09-04 09:25:58 +0000

    remove header nav

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -10,8 +10,6 @@ import {
   Container,
 } from '@chakra-ui/react';
 import ActiveLink from '../Link/ActiveLink';
-/* import {CloseIcon, MoonIcon, SunIcon} from '@chakra-ui/icons'
-import { HamburgerIcon } from '@chakra-ui/icons' */
 
 const Header = () => {
   /* const {colorMode, toggleColorMode} = useColorMode() */
@@ -42,82 +40,6 @@ const Header = () => {
           </Link>
         </Heading>
       </Flex>
-
-      <Box py="2" bgColor="rgba(200,200,200, 0.5)">
-        <Wrap
-          pb="2"
-          px="8"
-          spacing="4"
-          justify="center"
-          alignItems="center"
-          fontSize="18px"
-        >
-          <WrapItem pr="4">
-            <ActiveLink href={`${site}`} activeClassName="active">
-              <a>Home</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`/book`} activeClassName="active">
-              <a>Books & Manuscripts</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`${site}page/works`} activeClassName="active">
-              <a>Works</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`${site}page/people`} activeClassName="active">
-              <a>People</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`${site}page/places`} activeClassName="active">
-              <a>Places</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink
-              href={`${site}page/digital-editions`}
-              activeClassName="active"
-            >
-              <a>Digital editions</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={'/networks'} activeClassName="active">
-              <a>Networks</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={'https://revision-ontodia.vercel.app'} activeClassName="active">
-              <a>Ontodia</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`${site}page/bibliography`} activeClassName="active">
-              <a>Bibliography</a>
-            </ActiveLink>
-          </WrapItem>
-          <WrapItem pr="4">
-            <ActiveLink href={`/search`} activeClassName="active">
-              <a>Search</a>
-            </ActiveLink>
-          </WrapItem>
-
-          {/* <Button 
-            display={{base: 'none', md:'inherit'}}
-            mt="2"
-            p="0" 
-            h="5" 
-            w="1" 
-            onClick={toggleColorMode}
-          >
-            {colorMode === 'light' ? <MoonIcon w={3} h={3} /> : <SunIcon w={3} h={3} />}
-          </Button> */}
-        </Wrap>
-      </Box>
     </Container>
   );
 };
